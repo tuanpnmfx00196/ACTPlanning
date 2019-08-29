cbbe56102665006c8c2ea3507912a086a39e3aba
tuanpnm <phantuanqb85@gmail.com>
2019-08-29 18:32:26 +0700

    29 AUG

--- src/main/java/com/example/actplanning/SignUp.java
+++ src/main/java/com/example/actplanning/SignUp.java
@@ -1,13 +1,25 @@
 package com.example.actplanning;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SignUp extends AppCompatActivity {
-
+    Button btnBack;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+        btnBack = (Button)findViewById(R.id.btnBack);
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SignUp.this,MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
+
 }
